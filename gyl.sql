ee534659e433254b193173b6c35b1be6ebeb9f88
augusto <94504327+augustoba@users.noreply.github.com>
2023-05-18 20:27:36 -0300

    creacion de la tabla Usuario

--- gyl.sql
+++ gyl.sql
@@ -1,3 +1,11 @@
 create database if not exists gyl;
 
-create table if not exists Usuarios;
+create table if not exists Usuarios(
+ id bigint,
+ nombre varchar(20),
+ apellido varchar(20),
+ celular varchar(10),
+ correo varchar(15),
+ clave varchar(10)
+);
+
